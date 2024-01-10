1b2c9ef8fa445938c392bbc1489b6cab32c1a426
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-11 00:28:30 +0200

    function that returns a connector to the database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,8 +16,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    connection = mysql.connector.connect(
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    connection = mysql.connector.connection.MySQLConnection(
         host=db_host,
         user=db_user,
         password=db_pwd,
