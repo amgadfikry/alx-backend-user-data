ab33365d5dd303e957b23a175984267b41041bb8
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-11 01:53:01 +0200

    change some method with dealing with connector

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,7 +69,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", '')
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", 'holberton')
     connection = mysql.connector.connection.MySQLConnection(
         host=db_host,
         user=db_user,
