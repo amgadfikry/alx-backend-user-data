7de7f782363be4e3461216b6caa628f6f35fbd17
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-11 00:55:19 +0200

    add function that check if same password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
         and returns a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if password same as hashed password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
