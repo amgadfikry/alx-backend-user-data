8a453e588956d393f14127b10bf037d668d83936
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-16 21:30:00 +0200

    add method to basic auth that check if header contain valid base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ class inherit from auth class
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ check if header is valid base64 or not
+        """
+        if authorization_header and type(authorization_header) is str:
+            auth_head_split = authorization_header.split(' ')
+            if auth_head_split[0] == 'Basic':
+                return auth_head_split[1]
+        return None
