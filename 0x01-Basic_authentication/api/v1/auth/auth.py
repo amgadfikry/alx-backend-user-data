a1e60af5491b816be64f34f735870cfb512b6de4
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-16 11:03:14 +0200

    modify require_path to check if path in exclued path list

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,17 @@ class Auth:
     """ class of auth that contain all auth methods
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ method determine with path need auth
+        """ method determine with path in excluded path or not
+            Return:
+                True: if not in excluded path
+                False if path in excluded path
         """
-        return False
+        if path and (excluded_paths and len(excluded_paths) > 0):
+            if path[len(path) - 1] != '/':
+                path = path[:] + '/'
+            if path in excluded_paths:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ method which create auth header
