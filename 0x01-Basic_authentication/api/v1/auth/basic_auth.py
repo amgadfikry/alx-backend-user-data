11a33e1efe4d60a3f0f62a16c11fe0a3091b6680
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-16 21:44:44 +0200

    add method to basic auth that decode base64 to string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ module that handle basicauth class """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,4 +15,18 @@ class BasicAuth(Auth):
             auth_head_split = authorization_header.split(' ')
             if auth_head_split[0] == 'Basic':
                 return auth_head_split[1]
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ method that return decode64 of string
+        """
+        if base64_authorization_header\
+        and type(base64_authorization_header) is str:
+            try:
+                string = base64.b64decode(
+                    base64_authorization_header).decode('ascii')
+                return string
+            except Exception:
+                return None
         return None
