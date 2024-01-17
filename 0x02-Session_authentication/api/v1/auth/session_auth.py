016d61da24d6c39956530cd2eed6249ce670ac57
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-17 17:14:19 +0200

    create session class and modify app to create instance of it if use in env var

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" module that handle session auth class """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ class inherit from Auth and represent session auth
+    """
+    pass
