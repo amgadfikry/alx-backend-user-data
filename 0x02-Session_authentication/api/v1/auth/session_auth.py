89c1d9ee81f9af83906c2762c38a350255f3293a
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-17 18:49:52 +0200

    create current user method get user instance from cookies of request

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ module that handle session auth class """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +23,10 @@ class SessionAuth(Auth):
         """ method that retrive user id based on session id
         """
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
