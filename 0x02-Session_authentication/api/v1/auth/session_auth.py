1fe6b7ccaa2512d3f470d9d4cc9a74d0b1299843
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-17 17:24:16 +0200

    create new method in session class create_session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """ module that handle session auth class """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ class inherit from Auth and represent session auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ method that generate session id based on user id
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
