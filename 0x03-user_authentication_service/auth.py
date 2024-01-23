517f3eefcedb62d999da54c297e04fb95917a00e
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-23 11:50:33 +0200

    create new method hat check if valid login credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,13 @@ class Auth:
             new_user = self._db.add_user(email, _hash_password(password))
             return new_user
         raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ check if valid user and password
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                    password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False
