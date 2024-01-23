e13ce734e24f43413e4c91ff291e40f4f3c12ce3
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-23 12:18:28 +0200

    create route for login with session_id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ module for flask app """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -25,6 +25,20 @@ def register_user():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ login route to create session_id and sent with cookies
+    """
+    email, password = request.form.values()
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        res = make_response(
+                jsonify({"email": email, "message": "logged in"}))
+        res.set_cookie('session_id', session_id)
+        return res
+    abort(401)
 
 
 if __name__ == "__main__":
