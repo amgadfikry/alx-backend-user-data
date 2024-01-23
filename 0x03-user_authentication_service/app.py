3b05c28d72ec7475ea8cc601c1a835cab592a827
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-23 11:44:47 +0200

    create route users to check if register user or create new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ module for flask app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -11,6 +13,18 @@ def main_route():
     """ route to main
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """ route to check if email is register or create new user
+    """
+    email, password = request.form.values()
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
