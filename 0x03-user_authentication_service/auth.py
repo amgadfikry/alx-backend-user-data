c0fdc6ed6eaaf09659cc40e309cb17edcfe6716a
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-23 13:11:55 +0200

    add method update_password to update password using token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,10 +95,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            session_id = _generate_uuid()
             self._db.update_user(
                     user.id, hashed_password=_hash_password(password),
-                    reset_token=reset_token)
-            return session_id
+                    reset_token=None)
+            return None
         except Exception:
             raise ValueError
